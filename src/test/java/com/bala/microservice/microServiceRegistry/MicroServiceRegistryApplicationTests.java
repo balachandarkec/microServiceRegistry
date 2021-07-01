29b822e48ec95134683434d891bf5caaf92babe4
Balachandar <balachandar.s@niit.com>
2021-07-01 15:09:49 +0530

    Initial commit

---
+++ src/test/java/com/bala/microservice/microServiceRegistry/MicroServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.microservice.microServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
